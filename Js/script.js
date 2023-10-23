1cee4520bee38b57626f89b3c3e0c67d04ec2246
BrianSFarfan <brian.sfarfan@gmail.com>
2023-10-23 19:41:56 -0300

    xd

--- Js/script.js
+++ Js/script.js
@@ -7,3 +7,16 @@ console.log("El mes actual es:", nombreMesActual);
 const h1MesActual = document.getElementById("mes-actual");
 h1MesActual.textContent = nombreMesActual;
 
+
+
+function agregarGasto() {
+    var gasto = document.getElementById("gasto").value;
+    if (gasto) {
+        var listaGastos = document.getElementById("lista-gastos");
+        var nuevoElemento = document.createElement("tr");
+        nuevoElemento.textContent = gasto;
+        listaGastos.appendChild(nuevoElemento);
+  
+        document.getElementById("gasto").value = "";
+    }
+}
