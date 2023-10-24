575aa567b95604a42611bdce17b4f1d197b859b5
BrianSFarfan <brian.sfarfan@gmail.com>
2023-10-23 21:40:24 -0300

    se

--- Js/script.js
+++ Js/script.js
@@ -7,16 +7,85 @@ console.log("El mes actual es:", nombreMesActual);
 const h1MesActual = document.getElementById("mes-actual");
 h1MesActual.textContent = nombreMesActual;
 
-
+let total = 0;
 
 function agregarGasto() {
-    var gasto = document.getElementById("gasto").value;
-    if (gasto) {
-        var listaGastos = document.getElementById("lista-gastos");
-        var nuevoElemento = document.createElement("tr");
-        nuevoElemento.textContent = gasto;
-        listaGastos.appendChild(nuevoElemento);
-  
-        document.getElementById("gasto").value = "";
+    const nombre = document.getElementById("nombre").value;
+    const precio = parseFloat(document.getElementById("precio").value);
+
+    if (nombre && !isNaN(precio) && precio > 0) {
+        total += precio;
+        document.getElementById("total").textContent = total.toFixed(2);
+        // Puedes reiniciar los campos de nombre y precio si lo deseas
+        document.getElementById("nombre").value = "";
+        document.getElementById("precio").value = "";
+        
+        // También puedes agregar el nuevo gasto a la tabla si lo deseas
+        const listaGastos = document.getElementById("lista-gastos");
+        const newRow = listaGastos.insertRow();
+        newRow.insertCell().textContent = nombre;
+        newRow.insertCell().textContent = `$${precio.toFixed(2)}`;
+    } else {
+        alert("Por favor, ingresa un nombre válido y un precio mayor a 0.");
     }
+}
+
+let startX = 0;
+let startY = 0;
+let isSwiping = false;
+
+document.getElementById("lista-gastos").addEventListener("touchstart", handleTouchStart, false);
+document.getElementById("lista-gastos").addEventListener("mousedown", handleTouchStart, false);
+
+document.getElementById("lista-gastos").addEventListener("touchmove", handleTouchMove, false);
+document.getElementById("lista-gastos").addEventListener("mousemove", handleTouchMove, false);
+
+document.getElementById("lista-gastos").addEventListener("touchend", handleTouchEnd, false);
+document.getElementById("lista-gastos").addEventListener("mouseup", handleTouchEnd, false);
+
+function handleTouchStart(event) {
+    startX = event.clientX || event.touches[0].clientX;
+    startY = event.clientY || event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+    if (startX === 0 || startY === 0) {
+        return;
+    }
+
+    const currentX = event.clientX || event.touches[0].clientX;
+    const currentY = event.clientY || event.touches[0].clientY;
+
+    const deltaX = startX - currentX;
+    const deltaY = startY - currentY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        // Deslizamiento horizontal
+        isSwiping = true;
+    } else {
+        // Deslizamiento vertical, cancelar la operación de deslizamiento
+        isSwiping = false;
+    }
+}
+
+function handleTouchEnd(event) {
+    if (isSwiping) {
+        const deltaX = startX - (event.clientX || event.changedTouches[0].clientX);
+        if (deltaX > 50) {
+            // Se deslizó hacia la izquierda, eliminar la fila
+            const selectedRow = event.target.parentNode;
+            if (selectedRow.tagName.toLowerCase() === "tr") {
+                const precio = parseFloat(selectedRow.children[1].textContent.slice(1));
+                total -= precio;
+                selectedRow.classList.add("deslizar-izquierda"); // Agregar la clase de animación
+                selectedRow.innerHTML += '<td><button class="eliminar">Eliminar</button></td>'; // Agregar botón de eliminar
+                document.getElementById("total").textContent = total.toFixed(2);
+            }
+        }
+    }
+
+    // Restablecer los valores
+    startX = 0;
+    startY = 0;
+    isSwiping = false;
 }
